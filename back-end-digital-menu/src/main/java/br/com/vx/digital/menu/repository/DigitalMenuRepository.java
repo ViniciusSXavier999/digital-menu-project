ed219433d7d0373b69caaca68011e93c38fba7c1
ViniciusSXavier999 <viniciusxaviersouza11@gmail.com>
2024-05-17 11:18:05 -0300

    Criando DigitalMenuRepository

---
+++ back-end-digital-menu/src/main/java/br/com/vx/digital/menu/repository/DigitalMenuRepository.java
@@ -0,0 +1,7 @@
+package br.com.vx.digital.menu.repository;
+
+import br.com.vx.digital.menu.entity.DigitalMenu;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface DigitalMenuRepository extends JpaRepository<DigitalMenu, Long> {
+}
