3f022f8ce75cd77ad49a618edcacc8ea73adea20
ViniciusSXavier999 <viniciusxaviersouza11@gmail.com>
2024-05-31 03:52:10 -0300

    Desenvolvendo o componente Card

--- front-end-digital-menu/src/App.tsx
+++ front-end-digital-menu/src/App.tsx
@@ -1,12 +1,15 @@
 import './App.css'
+import { Card } from './components/card/card';
 
 function App() {
-
+  // Dados que vou pegar da minha API e fazer um get no front-end
+  // esse data vai representar os dados que vão vir da api
+  const data = [];
   return (
       <div className='container'>
         <h1>Cardápio</h1>
         <div className="card-grid">
-          
+          {data.map(menuData => <Card/>)}
         </div>
 
       </div>
