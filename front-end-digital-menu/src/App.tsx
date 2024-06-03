ae28da89992075059aaa5778030d0ef036bc817b
ViniciusSXavier999 <viniciusxaviersouza11@gmail.com>
2024-06-03 16:28:43 -0300

    Consumindo os dados da minha API do back-end e mostrando na tela

--- front-end-digital-menu/src/App.tsx
+++ front-end-digital-menu/src/App.tsx
@@ -1,15 +1,24 @@
 import './App.css'
 import { Card } from './components/card/card';
+import { useMenuData } from './hooks/useMenuData';
+import { MenuData } from './interface/MenuData';
 
 function App() {
   // Dados que vou pegar da minha API e fazer um get no front-end
   // esse data vai representar os dados que vão vir da api
-  const data = [];
+  const { data } = useMenuData();
+
   return (
       <div className='container'>
         <h1>Cardápio</h1>
         <div className="card-grid">
-          {data.map(menuData => <Card/>)}
+          {data?.map(menuData =>
+           <Card
+           price={menuData.price} 
+           title={menuData.title}
+           image={menuData.image}
+           />
+           )}
         </div>
 
       </div>
